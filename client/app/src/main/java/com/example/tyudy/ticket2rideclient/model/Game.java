55c1ac9e831833e1b07681f8937463997e305752
zacheat <zacheat@gmail.com>
2017-02-15 16:31:18 -0700

    Changed the client side game

--- client/app/src/main/java/com/example/tyudy/ticket2rideclient/model/Game.java
+++ client/app/src/main/java/com/example/tyudy/ticket2rideclient/model/Game.java
@@ -1,18 +1,53 @@
 package com.example.tyudy.ticket2rideclient.model;
 
 import java.util.ArrayList;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
- * Created by tyudy on 2/13/17.
+ * Created by colefox on 2/6/17.
  */
+public class Game implements Serializable
+{
+    private String ownerUsername;
+    private Set<Integer> players = new TreeSet<Integer>();
 
-public class Game {
-  ArrayList<User> players;
-  private Game(){
+    public Game()
+    {
+    }
 
-  }
-  public void addUser(User player){
-    players.add(player);
-  }
+    public void addPlayer(int playerID)
+    {
+        players.add(playerID);
+    }
+
+    public int getNumPlayers()
+    {
+        return players.size();
+    }
+
+    public String getOwnerUsername()
+    {
+        return ownerUsername;
+    }
+
+    public void setOwnerUsername(String ownerUsername)
+    {
+        this.ownerUsername = ownerUsername;
+    }
+
+    public Set<Integer> getPlayers()
+    {
+        return players;
+    }
+
+    public void setPlayers(Set<Integer> players)
+    {
+        this.players = players;
+    }
+}
+
 
 }

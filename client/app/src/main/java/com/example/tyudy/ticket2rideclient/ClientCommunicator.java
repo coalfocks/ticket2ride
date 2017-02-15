07d9e5bab5616f71eca05e0524d1a0b81ba2b9d2
coalfocks <coalfocks@gmail.com>
2017-02-15 14:46:29 -0700

    added CC logic from tests

--- client/app/src/main/java/com/example/tyudy/ticket2rideclient/ClientCommunicator.java
+++ client/app/src/main/java/com/example/tyudy/ticket2rideclient/ClientCommunicator.java
@@ -59,3 +59,55 @@ public class ClientCommunicator implements Runnable
         wait = seconds;
     }
 }
+
+
+//    DataTransferObject data = new DataTransferObject();
+//    User me = new User("cole","fox",1,0);
+//    String s = gson.toJson(me);
+//data.setData("15");
+//        data.setCommand("JoinGame");
+//        data.setPlayerID(1);
+//
+//        testcommand test = new testcommand();
+//        test.setData(data);
+//
+//        DataTransferObject dto = null;
+//        try
+//        {
+//        String urlString = "http://127.0.0.1:8080/command";
+//        URL url = new URL(urlString);
+//        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//        connection.setRequestMethod("POST");
+//        connection.setDoOutput(true);
+//        connection.connect();
+//
+//        String commandString = toString(test);
+//        OutputStream requestBody = connection.getOutputStream();
+//        requestBody.write(commandString.getBytes());
+//        requestBody.flush();
+//        requestBody.close();
+//
+//        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK)
+//        {
+//        InputStream responseBody = connection.getInputStream();
+//        ByteArrayOutputStream bouts = new ByteArrayOutputStream();
+//        byte [] buffer = new byte [1024];
+//        int length = 0;
+//        while ((length = responseBody.read(buffer)) != -1)
+//        {
+//        bouts.write(buffer, 0, length);
+//        }
+//
+//        String responseData = bouts.toString();
+//        dto = gson.fromJson(responseData, DataTransferObject.class);
+//        }
+//        else
+//        {
+//        dto = new DataTransferObject(data.getCommand(), "", "Could not connect!",null);
+//        System.out.print(dto.getErrorMsg());
+//        }
+//
+//        } catch (Exception e)
+//        {
+//        e.printStackTrace();
+//        }

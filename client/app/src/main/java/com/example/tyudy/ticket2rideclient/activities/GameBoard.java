28cf3171460757fce3e05013338cf07809095b75
tylerku <ty.udy22@gmail.com>
2017-02-22 14:37:01 -0700

    Create GameBoard activity
    ^^^

---
+++ client/app/src/main/java/com/example/tyudy/ticket2rideclient/activities/GameBoard.java
@@ -0,0 +1,15 @@
+package com.example.tyudy.ticket2rideclient.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tyudy.ticket2rideclient.R;
+
+public class GameBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_board);
+    }
+}

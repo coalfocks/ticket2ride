93f97890694d3f5374d81fc358e448e90643fc8d
zacheat <zacheat@gmail.com>
2017-03-09 11:27:10 -0700

    Game
    Game

--- client/app/src/main/java/com/example/tyudy/ticket2rideclient/common/commands/InitializeGameCommand.java
+++ client/app/src/main/java/com/example/tyudy/ticket2rideclient/common/commands/InitializeGameCommand.java
@@ -1,12 +1,9 @@
 package com.example.tyudy.ticket2rideclient.common.commands;
 
-import com.example.tyudy.ticket2rideclient.Serializer;
 import com.example.tyudy.ticket2rideclient.common.Command;
 import com.example.tyudy.ticket2rideclient.common.DataTransferObject;
-import com.example.tyudy.ticket2rideclient.common.TTRGame;
-import com.example.tyudy.ticket2rideclient.common.User;
+import com.example.tyudy.ticket2rideclient.common.TTRServerFacade;
 import com.example.tyudy.ticket2rideclient.common.iCommand;
-import com.example.tyudy.ticket2rideclient.model.ClientModel;
 
 import java.io.Serializable;
 
@@ -23,18 +20,7 @@ public class InitializeGameCommand extends Command implements iCommand, Serializ
 
     @Override
     public DataTransferObject execute() {
-        try {
-            TTRGame game = (TTRGame) Serializer.deserialize(data.getData());
-            game.setInProgress(1);
-            ClientModel.SINGLETON.setCurrentTTRGame(game);
-            for (User u : game.getUsers()) {
-                if (u.getPlayerID() == ClientModel.SINGLETON.getCurrentUser().getPlayerID()) {
-                    ClientModel.SINGLETON.setCurrentUser(u);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //update the current player
         return null;
     }
 }

6cb2646365eea285895a64cce2e0a6c14fda41f6
zacheat <zacheat@gmail.com>
2017-03-09 22:13:23 -0700

    One more thing
    Asdf

--- client/app/src/main/java/com/example/tyudy/ticket2rideclient/activities/PreGameActivity.java
+++ client/app/src/main/java/com/example/tyudy/ticket2rideclient/activities/PreGameActivity.java
@@ -14,8 +14,10 @@ import com.example.tyudy.ticket2rideclient.fragments.GameBoardFragment;
 import com.example.tyudy.ticket2rideclient.fragments.GameSelectionFragment;
 import com.example.tyudy.ticket2rideclient.fragments.LoginFragment;
 import com.example.tyudy.ticket2rideclient.common.User;
+import com.example.tyudy.ticket2rideclient.interfaces.iObserver;
+import com.example.tyudy.ticket2rideclient.model.ClientModel;
 
-public class PreGameActivity extends AppCompatActivity {
+public class PreGameActivity extends AppCompatActivity implements iObserver {
 
     private FragmentManager fm;
 
@@ -57,4 +59,15 @@ public class PreGameActivity extends AppCompatActivity {
         super.onResume();
         MethodsFacade.SINGLETON.setContext(this);
     }
+
+    @Override
+    public void observe() {
+
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        ClientModel.SINGLETON.removeObserver((iObserver) this);
+    }
+
 }

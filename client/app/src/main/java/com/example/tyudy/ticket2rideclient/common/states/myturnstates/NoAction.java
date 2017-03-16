48a499dc4f9137b283dd5fde67442eace511094b
Master Owl <cheesytrevor@gmail.com>
2017-03-15 19:04:43 -0600

    Added a current state field to the User class

--- client/app/src/main/java/com/example/tyudy/ticket2rideclient/common/states/myturnstates/NoAction.java
+++ client/app/src/main/java/com/example/tyudy/ticket2rideclient/common/states/myturnstates/NoAction.java
@@ -1,42 +1,43 @@
-package com.example.tyudy.ticket2rideclient.common.states;
+package com.example.tyudy.ticket2rideclient.common.states.myturnstates;
 
 import com.example.tyudy.ticket2rideclient.common.User;
 import com.example.tyudy.ticket2rideclient.common.cards.iCard;
+import com.example.tyudy.ticket2rideclient.common.states.MyTurnState;
 
 /**
  * Created by Trevor on 3/15/2017.
  */
 
-public class MyTurnState implements IState {
-    private User mUser;
+public class NoAction extends MyTurnState {
 
-    public MyTurnState(User currentUser) { mUser = currentUser; }
+    public NoAction(User currentUser) { super(currentUser);}
 
     @Override
     public boolean drawCard() {
-        return false;
+        return super.drawCard();
     }
 
     @Override
     public boolean drawDest() {
-        return false;
+        return super.drawDest();
     }
 
     @Override
     public boolean pickCard() {
-        return false;
+        return super.pickCard();
     }
 
     @Override
     public boolean claimPath() {
-        return false;
+        return super.claimPath();
     }
 
     @Override
     public boolean returnCard(iCard cardToBeReturned) {
-        return false;
+        return super.returnCard(cardToBeReturned);
     }
 
+    // You can't score points at the beginning of the turn
     @Override
     public boolean scorePoints() {
         return false;
